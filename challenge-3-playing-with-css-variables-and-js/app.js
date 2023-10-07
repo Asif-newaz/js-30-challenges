7e005987a6ca17f7f635c004981a3ced15d4d064
Asif-newaz <asif.newaz0093@gmail.com>
2023-10-08 03:41:15 +0600

    test: done with challenge 3

--- challenge-3-playing-with-css-variables-and-js/app.js
+++ challenge-3-playing-with-css-variables-and-js/app.js
@@ -1,9 +1,12 @@
 const inputs = document.querySelectorAll(".controls input");
 
-// console.log(input);
-
 function handleUpdate() {
-  console.log(this.value);
+  const suffix = this.dataset.sizing || "";
+
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
 }
 
 inputs.forEach((input) => input.addEventListener("change", handleUpdate));
